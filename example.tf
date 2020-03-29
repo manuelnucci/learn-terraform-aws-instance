a59e0248f65ac57703eb4617fbe4331a9deeae80
Manuel Nucci <manuucci96@gmail.com>
2020-03-29 10:05:12 -0300

    Initial infrastructure

---
+++ example.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+
+resource "aws_instance" "example" {
+  ami           = var.amis[var.region]
+  instance_type = "t2.micro"
+
+#   provisioner "local-exec" {
+    # command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+#   }
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
